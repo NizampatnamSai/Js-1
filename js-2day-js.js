5fb91b75c018e06f33bbc53b7f4fee2592d926f3
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2021-09-30 12:02:50 +0530

    first commit

---
+++ js-2day-js.js
@@ -0,0 +1,30 @@
+/*
+var ar=[2,87]
+console.log(ar)
+ar.length
+console.log(ar.length)
+ar.push(2,845)
+console.log(ar)
+const age=23;
+console.log(age)
+age=25;*/
+var add=2;
+console.log(++add)
+console.log('now add is',add)
+var add=2
+console.log(add++)
+
+console.log('now add is',add);
+var c=23;
+var d='23';
+console.log(c===d)
+// type casting
+console.log(c+d)
+var c1='dsd'
+var d1=343;
+console.log(c1+d1)
+var d1=865;
+var d1=85;
+var d1=8654;
+var d1=8654;
+
